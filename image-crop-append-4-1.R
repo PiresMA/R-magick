04668a5ce73ea711e4b2e62d8f4769e5bd99818c
PiresMA <pires.ma.fisica@gmail.com>
2020-05-16 22:50:14 -0300

    Add files via upload

--- image-crop-append-4-1.R
+++ image-crop-append-4-1.R
@@ -13,6 +13,5 @@ img_crp2 = image_crop(img_app2, "0x0+0-30")
 
 img_app3 = image_append(c(img_crp1,img_crp1) , stack = TRUE )
 
-image_write( img_app3, path = "image_crop.pdf", format = "pdf")
-
+image_write( img_app3, path = "image-crop-4-1.pdf", format = "pdf")
 
